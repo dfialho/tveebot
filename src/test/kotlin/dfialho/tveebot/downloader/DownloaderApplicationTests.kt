ef8e1132ff4d01da67e736fe2db61bb4319bc714
David Fialho <dfialho@protonmail.com>
2018-05-07 21:49:04 +0100

    Add project initial structure

---
+++ src/test/kotlin/dfialho/tveebot/downloader/DownloaderApplicationTests.kt
@@ -0,0 +1,16 @@
+package dfialho.tveebot.downloader
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class DownloaderApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
