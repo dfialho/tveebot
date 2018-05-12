2dd0c5067f1a5ddc6b8a01b16f75ac56e6322153
David Fialho <dfialho@protonmail.com>
2018-05-12 17:42:59 +0100

    Issue #6: Make application a spring boot application
    Initial integration with the spring framework. Given the configurations
    in the build.gradle file the application currently launches an HTTP
    server which is not being used right now.
    
    The parameters are now passed in using properties. This is only
    temporary.

--- src/main/kotlin/dfialho/tveebot/downloader/DownloaderApplication.kt
+++ src/main/kotlin/dfialho/tveebot/downloader/DownloaderApplication.kt
@@ -1,65 +1,90 @@
 package dfialho.tveebot.downloader
 
 import dfialho.tveebot.downloader.libtorrent.LibTorrentDownloadEngine
+import org.slf4j.LoggerFactory
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.stereotype.Component
 import java.nio.file.Files
-import java.nio.file.Paths
+import java.nio.file.Path
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 import kotlin.concurrent.thread
 import kotlin.system.exitProcess
 
-fun usage() {
-    println("Usage: ")
-    println("  downloader <save-directory> -f <torrent-file>")
-    println("  downloader <save-directory> -l <magnet-link>")
+@SpringBootApplication
+@EnableConfigurationProperties(DownloaderConfig::class)
+class DownloaderApplication {
+
+    @Bean
+    fun downloaderInitializer(downloader: Downloader) = CommandLineRunner {
+        downloader.download()
+    }
+
+    @Bean
+    fun downloaderManager(config: DownloaderConfig): DownloadManager {
+
+        if (!Files.isDirectory(config.savePath)) {
+            throw IllegalArgumentException("The download directory does not exist: ${config.savePath}")
+        }
+
+        return DownloadManager(LibTorrentDownloadEngine(config.savePath))
+    }
+
+}
+
+@ConfigurationProperties("downloader")
+class DownloaderConfig {
+
+    lateinit var savePath: Path
+    lateinit var magnetLink: String
+}
+@Component
+class Downloader(private val config: DownloaderConfig, private val downloadManager: DownloadManager) {
+
+    companion object {
+        val logger = LoggerFactory.getLogger(Downloader::class.java)
+    }
+
+    val exitLatch = CountDownLatch(1)
+
+    init {
+        downloadManager.start()
+
+        Runtime.getRuntime().addShutdownHook(thread(start = false) {
+            logger.info("Closing downloader")
+            downloadManager.stop()
+            logger.info("Downloader closed")
+        })
+
+        val listener: EventListener = object : EventListener {
+            override fun onDownloadFinished(reference: DownloadReference) {
+                logger.info("Download is complete")
+                exitLatch.countDown()
+            }
+        }
+
+        downloadManager.engine.addListener(listener)
+    }
+
+    fun download() {
+        val downloadHandle = downloadManager.engine.add(config.magnetLink)
+
+        while (!exitLatch.await(1, TimeUnit.SECONDS)) {
+            val status = downloadHandle.getStatus()
+
+            logger.info("${status.name} (${status.state}): %.2f%% - ${status.rate / 1000} kB/s".format(status.progress * 100))
+        }
+
+        exitProcess(0)
+    }
+
 }
 
 fun main(args: Array<String>) {
-
-    if (args.size != 3) {
-        usage()
-        exitProcess(1)
-    }
-
-    val savePath = Paths.get(args[0])
-    val option: String = args[1]
-    val torrentReference: String = args[2]
-
-    if (!Files.isDirectory(savePath)) {
-        println("The download directory does not exists: $savePath")
-        exitProcess(1)
-    }
-
-    val downloadManager = DownloadManager(LibTorrentDownloadEngine(savePath))
-    downloadManager.start()
-
-    Runtime.getRuntime().addShutdownHook(thread(start = false) {
-        println()
-        print("Closing...  ")
-        downloadManager.stop()
-        println("done!")
-    })
-
-    val exitLatch = CountDownLatch(1)
-    val listener: EventListener = object : EventListener {
-        override fun onDownloadFinished(reference: DownloadReference) {
-            println("Download is complete")
-            exitLatch.countDown()
-        }
-    }
-
-    downloadManager.engine.addListener(listener)
-
-    val downloadHandle = when (option) {
-        "-f" -> downloadManager.engine.add(torrentFile = Paths.get(torrentReference))
-        "-l" -> downloadManager.engine.add(magnetLink = torrentReference)
-        else -> { usage(); exitProcess(1) }
-    }
-
-    while (!exitLatch.await(1, TimeUnit.SECONDS)) {
-        val status = downloadHandle.getStatus()
-
-        println("${status.name} (${status.state}): %.2f%% - ${status.rate / 1000} kB/s".format(status.progress * 100))
-    }
-
+    runApplication<DownloaderApplication>()
 }
