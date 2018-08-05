3e900fd66f88bf3de938e00e43346c2a5f1250d6
David Fialho <dfialho@protonmail.com>
2018-08-05 12:21:56 +0100

    Add RSSFeedReader to parse the TV Shows' RSS feeds

---
+++ common-utils/src/main/kotlin/dfialho/tveebot/utils/rssfeed/RSSFeedReader.kt
@@ -0,0 +1,65 @@
+package dfialho.tveebot.utils.rssfeed
+
+import com.sun.syndication.feed.synd.SyndEntry
+import com.sun.syndication.feed.synd.SyndFeed
+import com.sun.syndication.io.SyndFeedInput
+import com.sun.syndication.io.XmlReader
+import java.io.File
+import java.io.FileInputStream
+import java.io.InputStream
+import java.net.URL
+import java.nio.file.Path
+
+/**
+ *
+ * The [RSSFeedReader] is a wrapper around RSS parser implementation. It provides an abstraction for the RSS feed
+ * parsing library. It is able to read RSS feeds from an XML document. The document can be passed in as a [File],
+ * [URL], or [InputStream].
+ *
+ * @author David Fialho (dfialho@protonmail.com)
+ */
+class RSSFeedReader {
+
+    /**
+     * Internal feed parser.
+     */
+    private val syndFeedInput = SyndFeedInput()
+
+    /**
+     * Reads an RSS feed from a [file] and returns the result in a [RSSFeed].
+     */
+    fun read(file: File): RSSFeed {
+        return syndFeedInput.build(XmlReader(file)).toRSSFeed()
+    }
+
+    /**
+     * Reads an RSS feed from an [URL] and returns the result in a [RSSFeed].
+     */
+    fun read(url: URL): RSSFeed {
+        return syndFeedInput.build(XmlReader(url)).toRSSFeed()
+    }
+
+    /**
+     * Reads an RSS feed from an [InputStream] and returns the result in a [RSSFeed].
+     */
+    fun read(inputStream: InputStream): RSSFeed {
+        return syndFeedInput.build(XmlReader(inputStream)).toRSSFeed()
+    }
+}
+
+/**
+ * Converts a [SyndFeed] into an [RSSFeed] instance and returns the result.
+ */
+private fun SyndFeed.toRSSFeed(): RSSFeed {
+    @Suppress("UNCHECKED_CAST")
+    val syndEntries = this.entries as List<SyndEntry>
+
+    return RSSFeed(
+        title = this.title.orEmpty(),
+        items = syndEntries.map { RSSFeedItem(
+            it.title.orEmpty(),
+            it.link.orEmpty(),
+            it.publishedDate.toInstant())
+        }
+    )
+}
