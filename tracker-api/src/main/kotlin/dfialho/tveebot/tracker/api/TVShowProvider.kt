38e46f995211c5b604a49b404ea697cbb066effb
David Fialho <dfialho@protonmail.com>
2018-08-05 16:49:30 +0100

    Move dependency 'org.jetbrains.kotlin:kotlin-stdlib-jdk8' to root project

--- tracker-api/src/main/kotlin/dfialho/tveebot/tracker/api/TVShowProvider.kt
+++ tracker-api/src/main/kotlin/dfialho/tveebot/tracker/api/TVShowProvider.kt
@@ -1,6 +1,15 @@
 package dfialho.tveebot.tracker.api
 
+/**
+ * A TV show provider exposes methods to fetch video files for TV show episodes from a repository.
+ *
+ * @author David Fialho (dfialho@protonmail.com)
+ */
 interface TVShowProvider {
 
+    /**
+     * Fetches the most recent episodes available for [tvShow]. The number of episodes returned by this method is
+     * specific to each implementation.
+     */
     fun fetchEpisodes(tvShow: TVShow): List<EpisodeVideo>
 }
