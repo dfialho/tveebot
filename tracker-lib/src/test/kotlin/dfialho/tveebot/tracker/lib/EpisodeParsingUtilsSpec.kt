7c7486f8f315a1be5d677692427ba4dbd63ef76b
David Fialho <dfialho@protonmail.com>
2018-09-03 18:53:12 +0100

    Issue #33: Port tests to Spek 2

--- tracker-lib/src/test/kotlin/dfialho/tveebot/tracker/lib/EpisodeParsingUtilsSpec.kt
+++ tracker-lib/src/test/kotlin/dfialho/tveebot/tracker/lib/EpisodeParsingUtilsSpec.kt
@@ -3,10 +3,8 @@ package dfialho.tveebot.tracker.lib
 import dfialho.tveebot.tracker.api.Episode
 import dfialho.tveebot.tracker.api.VideoQuality
 import org.amshove.kluent.shouldEqual
-import org.jetbrains.spek.api.Spek
-import org.jetbrains.spek.api.dsl.context
-import org.jetbrains.spek.api.dsl.describe
-import org.jetbrains.spek.api.dsl.it
+import org.spekframework.spek2.Spek
+import org.spekframework.spek2.style.specification.describe
 import kotlin.test.assertFailsWith
 
 /**
