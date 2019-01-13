5b15bf7a28af8d4b6c609d687a189a2e60e1036a
David Fialho <dfialho@protonmail.com>
2019-01-13 08:45:03 +0000

    Issue #49: Library path constructed using the output location

--- library-lib/src/main/kotlin/dfialho/tveebot/library/lib/SimpleTVShowLibrary.kt
+++ library-lib/src/main/kotlin/dfialho/tveebot/library/lib/SimpleTVShowLibrary.kt
@@ -14,12 +14,13 @@ class SimpleTVShowLibrary(private val libraryDirectory: Path, private val organi
     override fun store(episode: TVShowEpisode, episodePackage: EpisodePackage) {
 
         val episodeLocation = organizer.getLocationOf(episode)
-        val extension = GFiles.getFileExtension(episodeLocation.fileName.toString())
+        val episodePath = episodePackage.getEpisode()
+        val extension = GFiles.getFileExtension(episodePath.fileName.toString())
         val destinationSubPath = episodeLocation.resolveSibling("${episodeLocation.fileName}.$extension")
         val destinationPath = libraryDirectory.resolve(destinationSubPath)
 
         Files.createDirectories(destinationPath.parent)
-        Files.move(episodeLocation, destinationPath, StandardCopyOption.REPLACE_EXISTING)
+        Files.move(episodePackage.getEpisode(), destinationPath, StandardCopyOption.REPLACE_EXISTING)
         episodePackage.path.toFile().deleteRecursively()
     }
 }
