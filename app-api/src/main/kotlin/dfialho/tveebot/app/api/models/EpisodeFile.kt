a429e3963493493c61d451fb3e591dc8d6cf8b76
David Fialho <dfialho@protonmail.com>
2020-04-12 09:09:37 +0100

    Tweaks: Ensure all episodes in a file have the same tv show

--- app-api/src/main/kotlin/dfialho/tveebot/app/api/models/EpisodeFile.kt
+++ app-api/src/main/kotlin/dfialho/tveebot/app/api/models/EpisodeFile.kt
@@ -3,4 +3,8 @@ package dfialho.tveebot.app.api.models
 data class EpisodeFile(
     val file: VideoFile,
     val episodes: List<Episode>
-)
+) {
+    init {
+        require(episodes.map { it.tvShow }.toSet().size == 1) { "All episodes must have the same TV Show" }
+    }
+}
